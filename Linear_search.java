1804737b80c8ce53b6806797d43c5b4108d96809
Vishnuvardhan K <93212616+Vishnu5090@users.noreply.github.com>
2025-03-27 23:08:46 +0530

    Update Linear_search.java

--- Linear_search.java
+++ Linear_search.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Linear_search{
     public static void main(String args[]){
         System.out.println("Linear_search :");
-        int a[]={10,20,40,50};
+        int a[]={10,20,40,50,60,70,80,90,100};
         int key=50;
         for(int i=0;i < a.length; i++){
             if(a[i]==key){
