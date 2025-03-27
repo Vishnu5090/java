eb3e97e47d45b1c32f11e8426724fadb44e79f3c
Vishnu <vish.2005.in@gmail.com>
2025-03-27 23:00:41 +0530

    2 commite

--- Linear_search.java
+++ Linear_search.java
@@ -4,7 +4,7 @@ class Linear_search{
     public static void main(String args[]){
         System.out.println("Linear_search");
         int a[]={10,20,40,50};
-        int key=40;
+        int key=50;
         for(int i=0;i < a.length; i++){
             if(a[i]==key){
                 System.out.println(i);
