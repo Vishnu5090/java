2f7abdccab44593f6875d82a939dd7c11a503597
Vishnu <vish.2005.in@gmail.com>
2025-03-27 22:56:17 +0530

    Linear search program

---
+++ Linear_search.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Linear_search{
+    public static void main(String args[]){
+        System.out.println("Linear_search");
+        int a[]={10,20,40,50};
+        int key=40;
+        for(int i=0;i < a.length; i++){
+            if(a[i]==key){
+                System.out.println(i);
+            }
+        }
+    }
+}
